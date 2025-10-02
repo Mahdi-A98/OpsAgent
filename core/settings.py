f394e8df43cf07875a29105f8a9f9ff494ae632a
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2025-10-02 20:16:27 +0330

    fix: settings path variables

--- core/settings.py
+++ core/settings.py
@@ -1,12 +1,14 @@
 import os
-from dotenv import load_dotenv
 from pathlib import Path
+from dotenv import load_dotenv
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv("core/.env")
+load_dotenv(BASE_DIR / ".env")
+load_dotenv(BASE_DIR / "core/.env")
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 TAVILY_API_KEY = os.environ.get("TAVILY_API_KEY")
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 GOOGLE_SEARCH_ENGINE_ID = os.environ.get("GOOGLE_SEARCH_ENGINE_ID")
-FAISS_INDEX_PATH = Path("faiss_index")
+FAISS_INDEX_PATH = BASE_DIR / "data/faiss_index"
