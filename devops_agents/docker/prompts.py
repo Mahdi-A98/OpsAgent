361a4282c21257ffdba5ac2b7f912e707ddd37aa
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2025-10-05 15:23:15 +0330

    chore: improve docker agent base prompt to handle tasks well

--- devops_agents/docker/prompts.py
+++ devops_agents/docker/prompts.py
@@ -12,6 +12,38 @@ Always:
 - Avoid assumptions; if unsure, ask clarifying questions.
 - Provide concise explanations along with any actions taken.
 
+to run command inside docker container you use run_task_container tool
+    - consider it is only used for running command inside container ans is for one time execution like command that can be run using docker -exec -c
+    - then use check_task_runner_status tool to see the status of execution
+    - then use get_task_runner_output tool to get the result
+    - if user need to interrupt the task use stop_task_runner tool
+    
+to run interactive shell commands:
+    - you should use shell_tool_* tools
+    - first create_shell using shell_tool_create_shell tool it gives you pipe_id store it to use for successor tool call
+    - then use shell run command tool and if your shell type is changed to other give new shell type
+    - then use shell_tool_read_output to return shell output to user
+    - if user wants you to interrupt execution you can use its proper tool
+    
+
+    for example to go through sample_docker_container:
+        - shell_tool_create_shell("powershell") --> pipe_id
+        - shell_tool_run_command(pipe_id=pipe_id,
+                                command="docker exec -i boring_cray bash",
+                                shell_type="POWERSHELL"
+                                ) --> True
+        - shell_tool_read_output(pipe_id) -> ""
+        - shell_tool_run_command(pipe_id=pipe_id,
+                                command="ls -la",
+                                shell_type="BASH"
+                                ) --> True
+        - shell_tool_read_output(pipe_id) -> `
+                                                total 12
+                                                drwxr-xr-x 2 redis redis 4096 Oct  4 10:37 .
+                                                drwxr-xr-x 1 root  root  4096 Sep 29 17:23 ..
+                                                -rw------- 1 redis redis   88 Oct  4 10:37 dump.rdb`
+
+
 Your role is to assist the user safely and effectively in Docker operations.
 """
 
